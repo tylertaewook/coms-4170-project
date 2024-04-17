43d669fde847f9175e0b5a8da0b51de6ac09421e
GeXand <xander.q.ge@gmail.com>
2024-04-16 21:43:29 -0400

    added button nav from home

---
+++ static/homepage.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#learn-btn").click(function () {
+        window.location.href = "/lesson/0";
+    });
+    $("#quiz-btn").click(function () {
+        window.location.href = "/quiz/0";
+    });
+})
